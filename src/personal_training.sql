5d172e27912bb77773eb897870c9e29f14ac54a2
Alex Evans <alex.evans@ada.ac.uk>
2025-02-10 16:05:00 +0000

    Personal Training DML Finished

--- src/personal_training.sql
+++ src/personal_training.sql
@@ -1,6 +1,7 @@
 -- Initial SQLite setup
 .open fittrackpro.db
 .mode column
+.mode box
 
 -- Enable foreign key support
 
@@ -8,3 +9,9 @@
 
 -- 1. List all personal training sessions for a specific trainer
 -- TODO: Write a query to list all personal training sessions for a specific trainer
+SELECT pts.session_id, m.first_name AS member_name, pts.session_date, pts.start_time, pts.end_time
+FROM members m 
+INNER JOIN personal_training_sessions pts ON m.member_id = pts.member_id 
+WHERE pts.staff_id = (SELECT staff_id
+                      FROM staff
+                      WHERE first_name == 'Ivy' AND  last_name == 'Irwin')
